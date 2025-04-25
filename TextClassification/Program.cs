d88a270489424b22df9923443b6ec9f7758b444f
M Reza Faisal <reza.faisal@gmail.com>
2025-04-25 13:19:51 +0800

    update

---
+++ TextClassification/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TextClassification;
+
+Console.WriteLine("Klasifikasi Teks");
+
+//Load sample data
+var sampleData = new AnalisisSentimen.ModelInput()
+{
+    Text = @"Film king Arthur ini film paling jelek dari seluruh cerita King Arthur",
+};
+
+//Load model and predict output
+var result = AnalisisSentimen.Predict(sampleData);
+
